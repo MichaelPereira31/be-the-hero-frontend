4b20afd7d06e4b43389413788c1ef684debe9961
oopaze <pedroosd28@gmail.com>
2023-05-22 00:54:42 -0300

    refactor: improving input implementation

--- src/components/Inputs/index.tsx
+++ src/components/Inputs/index.tsx
@@ -1,40 +1,44 @@
-import { useState } from 'react'
+import { InputHTMLAttributes, useState } from 'react'
 
 import * as S from './styles'
 
 import Eye from '../../assets/img/eye.png'
 import CloseEye from '../../assets/img/close-eye.png'
 
-type TInputsProps = {
-  type: string
-  name: string
-  showButton: boolean
+interface TInputsProps extends InputHTMLAttributes<HTMLInputElement> {
+  showButton?: boolean
+  error?: string
+  label?: string
 }
 
 const Input = (props: TInputsProps) => {
-  const { name, type, showButton = false } = props
+  const { name, label, type, showButton = false, error, ...rest } = props
   const [inputType, setInputType] = useState(type)
   const validatePassword = inputType === 'password'
 
   return (
-    <S.Wrapper data-testid={`input-validated-${name}`}>
-      <S.Input
-        name={name}
-        type={inputType}
-        required
-        data-testid={`input-validated-test`}
-      />
-      {showButton && (
-        <S.Image
-          src={validatePassword ? Eye.src : CloseEye.src}
-          alt="Mostrar senha"
-          onClick={() =>
-            validatePassword ? setInputType('text') : setInputType('password')
-          }
-          data-testid={'show-password-button'}
+    <>
+      {label && <S.Label>{label}</S.Label>}
+      <S.Wrapper data-testid={`input-validated-${name}`}>
+        <S.Input
+          name={name}
+          type={inputType}
+          data-testid={`input-validated-test`}
+          {...rest}
         />
-      )}
-    </S.Wrapper>
+        {showButton && (
+          <S.Image
+            src={validatePassword ? Eye.src : CloseEye.src}
+            alt="Mostrar senha"
+            onClick={() =>
+              validatePassword ? setInputType('text') : setInputType('password')
+            }
+            data-testid={'show-password-button'}
+          />
+        )}
+        {error && <S.Error>{error}</S.Error>}
+      </S.Wrapper>
+    </>
   )
 }
 
