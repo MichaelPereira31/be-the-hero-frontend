6afe08cb30f5989c1ffc6d0d4a36db74ddb1b1d5
oopaze <pedroosd28@gmail.com>
2023-05-22 00:56:10 -0300

    feat: adding font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,18 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import { Html, Head, Main, NextScript } from 'next/document'
 
 const Document = () => {
   return (
     <Html lang="pt-br">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:wght@100;200;300;400;500;600;700&family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
